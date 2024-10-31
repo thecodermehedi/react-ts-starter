41153b6565ac3eb09ec840b26f4e910ad02f6cc2
Mehedi Hasan <thecodermehedi@gmail.com>
2024-11-01 00:02:18 +0600

    added alias functionality

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import '@/styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
  <StrictMode>
-  <main className='min-h-screen flex justify-center items-center text-3xl font-bold'>
-   <p className='border-black border-b-2 leading-10'>React Starter Template</p>
+  <main className="min-h-screen flex justify-center items-center text-3xl font-bold">
+   <p className="border-black border-b-2 leading-10">React Starter Template</p>
   </main>
- </StrictMode>,
-)
+ </StrictMode>
+);

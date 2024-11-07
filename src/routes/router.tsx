dfd470a667102964a370510af6daef8e9eb1ff4b
Mehedi Hasan <mehedi@etlimited.net>
2024-11-07 12:40:04 +0600

    feat: create router.tsx

---
+++ src/routes/router.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from 'react-router-dom';
+import RootLayout from "@/layouts/RootLayout";
+import Home from "@/pages/Home";
+
+const router = createBrowserRouter([
+ {
+  path: "/",
+  element: <RootLayout />,
+  children: [
+   {
+    index: true,
+    element: <Home />
+   }
+  ]
+ }
+])
+
+export default router;

823af21a0f07862ecac76fb95ec1418c9b529a60
Mehedi Hasan <mehedi@etlimited.net>
2024-11-07 12:39:46 +0600

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import router from '@/routes/router';
 import '@/styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
  <StrictMode>
-  <main className="min-h-screen flex justify-center items-center text-3xl font-bold">
-   <p className="border-black border-b-2 leading-10">React Starter Template</p>
-  </main>
+  <RouterProvider router={router} />
  </StrictMode>
 );

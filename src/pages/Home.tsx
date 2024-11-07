524965a01947fa1802d127cfe505cc1065d1c480
Mehedi Hasan <mehedi@etlimited.net>
2024-11-07 12:40:01 +0600

    feat: create Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = (): JSX.Element => {
+ return (
+  <main className="min-h-screen flex justify-center items-center text-3xl font-bold">
+   <p className="border-black border-b-2 leading-10">React Starter Template</p>
+  </main>
+ )
+}
+
+export default Home
+
